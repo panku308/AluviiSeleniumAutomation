a0c36a7d6c3263eca603139ad571ee88b9d11fe5
DELL <DELL@DESKTOP-TRUTT1L>
2018-06-27 16:50:29 +0530

    Report customization class added.

---
+++ src/GlobalFiles/ReportCustomization.java
@@ -0,0 +1,93 @@
+package GlobalFiles;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ReportCustomization implements ITestListener {
+	@Override
+
+    public void onStart(ITestContext test) {
+
+         System.out.println("Start Of Execution(TEST)->"+test.getName());
+
+    }
+
+    @Override
+
+    public void onTestStart(ITestResult test) {
+
+        System.out.println("Test Started->"+test.getName());
+
+    }
+
+    @Override
+
+    public void onTestSuccess(ITestResult test) {
+
+        System.out.println("Test Pass->"+test.getName());
+        try
+        {
+        	CommonFunctions.TakeScreenShot(test.getName());
+        }
+        catch(Exception e)
+        {
+        	
+        }
+        
+
+    }
+
+    @Override
+
+    public void onTestFailure(ITestResult test) {
+
+        System.out.println("Test Failed->"+test.getName());
+        try
+        {
+        	CommonFunctions.TakeScreenShot(test.getName());
+        }
+        catch(Exception e)
+        {
+        	
+        }
+
+    }
+
+    @Override
+
+    public void onTestSkipped(ITestResult test) {
+
+        System.out.println("Test Skipped->"+test.getName());
+        try
+        {
+        	CommonFunctions.TakeScreenShot(test.getName());
+        }
+        catch(Exception e)
+        {
+        	
+        }
+
+    }
+
+    @Override
+
+    public void onFinish(ITestContext test) {
+
+System.out.println("END Of Execution(TEST)->"+test.getName());
+
+    }
+
+    @Override
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult test) {
+
+        // TODO Auto-generated method stub
+
+        
+
+    }
+
+
+
+}
