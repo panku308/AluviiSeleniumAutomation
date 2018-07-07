59194a0cfec2a4a3c263bc5f7de3ae193c465898
DELL <DELL@DESKTOP-TRUTT1L>
2018-07-07 11:55:32 +0530

    change card processor to knox and USAePay related code done.

---
+++ src/Tests/AdministrationTest/ChangeCardProcessorToKnox.java
@@ -0,0 +1,39 @@
+package Tests.AdministrationTest;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import GlobalFiles.CommonFunctions;
+import ObjectRepository.AdministrationDashboardPageElements;
+import ObjectRepository.DashboardPageElements;
+
+public class ChangeCardProcessorToKnox {
+	
+	public static WebDriver driver=null;
+
+	@BeforeClass
+	public void beforeClass() throws InterruptedException {
+		driver = CommonFunctions.driver;
+	}
+	@Test
+	public void ChangeCardProcessorToCardKnox() throws InterruptedException
+	{
+		CommonFunctions.ScrollUptoElement(driver, DashboardPageElements.GetAdministrationLink(driver));
+		Thread.sleep(2000);
+		DashboardPageElements.GetAdministrationLink(driver).click();
+		Thread.sleep(3000);
+		AdministrationDashboardPageElements.GetConfigurationTab(driver).click();
+		Thread.sleep(3000);
+		AdministrationDashboardPageElements.GetConfigTab_GatewayProcessorDD(driver).click();
+		Thread.sleep(2000);
+		CommonFunctions.SelectOptionFromDropdownList(driver, "CardKnox");
+		AdministrationDashboardPageElements.GetConfigTab_GatewayActionURLField(driver).clear();
+		AdministrationDashboardPageElements.GetConfigTab_GatewayActionURLField(driver).sendKeys("https://x1.cardknox.com/gateway");
+		AdministrationDashboardPageElements.GetConfigTab_GatewayOnlineKeyField(driver).clear();
+		AdministrationDashboardPageElements.GetConfigTab_GatewayOnlineKeyField(driver).sendKeys("AluviiDev_Test");
+		AdministrationDashboardPageElements.GetConfigTab_SaveChangesButton(driver).click();
+		Thread.sleep(5000);
+		
+	}
+}
