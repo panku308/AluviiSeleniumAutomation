60ae0461ac9788547be83d19af002ea14cc0d5be
DELL <DELL@DESKTOP-TRUTT1L>
2018-08-08 09:35:14 +0530

    Booking related code done partial.

--- src/GlobalFiles/Debug.java
+++ src/GlobalFiles/Debug.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.server.handler.SendKeys;
 
 import ObjectRepository.AddCategoryDialogBoxInsideEcommerceProductsTabElements;
 import ObjectRepository.AddModifierDialogBoxElements;
@@ -22,12 +23,15 @@ import ObjectRepository.AddModifierDialogBoxInsideAssignProductModifer;
 import ObjectRepository.AddProductDialogBoxElements;
 import ObjectRepository.AddProductDialogboxInsideAddRegisterPageElements;
 import ObjectRepository.AssignProductModifiersDialogBoxElements;
+import ObjectRepository.BookingDashboardPageElements;
+import ObjectRepository.BookingEventDashboardPageElements;
 import ObjectRepository.CashierRegisterPageElements;
 import ObjectRepository.CategoryDialogBoxElements;
 import ObjectRepository.DashboardPageElements;
 import ObjectRepository.ECommerceDashboardPageElements;
 import ObjectRepository.ManageMembershipsPageElements;
 import ObjectRepository.ProductModifierInsideCashierRegisterElements;
+import ObjectRepository.ReceiptSelectionDialogBoxElements;
 import ObjectRepository.RegisterListPageElements;
 import ObjectRepository.RegisterManagementDashboardPageElements;
 import Tests.RegisterManagementTest.CreateCategoryTest;
@@ -39,65 +43,122 @@ import util.DriverManager;
 public class Debug {
 	public static WebDriver driver=null;
 	public static String ModifierName="";
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws Exception {
-		String home = System.getProperty("user.home");
-		System.out.println(home);
-		  driver = CommonFunctions.SetupEnvironment(CommonFunctions.url, "chrome");
-		  Thread.sleep(5000);
-		  CommonFunctions.Login(driver, CommonFunctions.UserName, CommonFunctions.Password);
-		  Thread.sleep(5000);
-		  CommonFunctions.ScrollUptoElement(driver, DashboardPageElements.GetEcommerceLink(driver));
-		  Thread.sleep(2000);
-		  DashboardPageElements.GetEcommerceLink(driver).click();
-		  Thread.sleep(5000);
-		  ECommerceDashboardPageElements.GetProductsTab(driver).click();
-		  Thread.sleep(2000);
-		  ECommerceDashboardPageElements.GetProductsTab_SubTab_Categories(driver).click();
-		  Thread.sleep(3000);
-		  ECommerceDashboardPageElements.GetProductsTab_SubTab_Categories_SelectOptionDD(driver).click();
-		  Thread.sleep(2000);
-		  RegisterManagementDashboardPageElements.SelectOptionFromSelectOptionDD(driver, "Add Category").click();
-		  Thread.sleep(3000);
-		  CommonFunctions.SwitchToContentFrame(driver);
-		  AddCategoryDialogBoxInsideEcommerceProductsTabElements.GetUploadButton(driver).click();
-		  Thread.sleep(5000);
-		  CommonFunctions.SwitchToContentFrame(driver);
-		  CommonFunctions.ActionBuilder_PerformClickEventOnElement(driver, AddCategoryDialogBoxInsideEcommerceProductsTabElements.GetSelectfilesButton(driver));
+		Date dt = new Date();
+		System.out.println(dt.getDay());
+		driver = CommonFunctions.SetupEnvironment(CommonFunctions.url, "chrome");
+		Thread.sleep(5000);
+		CommonFunctions.Login(driver, CommonFunctions.UserName, CommonFunctions.Password);
+		Thread.sleep(5000);
+		String time="1:00pm";  
 		  
-		  Thread.sleep(3000);
-		  CommonFunctions.UploadFile(System.getProperty("user.dir") + "\\resources\\index.jpg");
-		  Thread.sleep(30000);
+		  try
+		  {
+			  CommonFunctions.ScrollUptoElement(driver, DashboardPageElements.GetBookingsLink(driver));
+				DashboardPageElements.GetBookingsLink(driver).click();
+				Thread.sleep(5000);
+				
+				BookingEventDashboardPageElements.GetOrderIDColumnSettingIcon(driver).click();
+				filterTable(driver, "23060");
+				Thread.sleep(5000);
+				
+				CommonFunctions.ActionBuilder_PerformMouseHoverEventOnElement(driver, BookingEventDashboardPageElements.GetBookingPlusIcon(driver));
+				Thread.sleep(2000);
+				BookingEventDashboardPageElements.GetAddBookingOption(driver).click();
+				Thread.sleep(5000);
+				
+				
+				BookingDashboardPageElements.Get_CategoryTab_CategoryDropdown(driver).click();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.SelectCategory(driver, "Brandon Party");
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_CategoryTab_Package_SelectButton(driver).click();
+				Thread.sleep(30000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TimeField(driver, time).click();
+				Thread.sleep(5000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield(driver).click();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield(driver).clear();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield(driver).sendKeys("10");
+				
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield2(driver).click();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield2(driver).clear();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield2(driver).sendKeys("25");
+				
+				
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield3(driver).click();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield3(driver).clear();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield3(driver).sendKeys("5");
+				
+				BookingDashboardPageElements.Get_BookingDetailsTab_TicketQtyfield3(driver).sendKeys(Keys.TAB);
+				
+				CommonFunctions.ScrollUptoElement(driver, BookingDashboardPageElements.GetBookingDataTab(driver));
+				Thread.sleep(3000);
+				BookingDashboardPageElements.GetBookingDataTab(driver).click();
+				Thread.sleep(2000);
+				BookingDashboardPageElements.Get_BookingDataTab_FirstNameField(driver).sendKeys("fname"+System.currentTimeMillis());
+				BookingDashboardPageElements.Get_BookingDataTab_LastNameField(driver).sendKeys("lname"+System.currentTimeMillis());
+				BookingDashboardPageElements.Get_BookingDataTab_EmailField(driver).sendKeys("email"+System.currentTimeMillis());
+				BookingDashboardPageElements.Get_BookingDataTab_PhoneField(driver).sendKeys("1234567890");
+				BookingDashboardPageElements.Get_BookingDataTab_CreateAccountButton(driver).click();
+				Thread.sleep(5000);
+				BookingDashboardPageElements.Get_BookingDataTab_CreateAccountButton(driver).click();
+				Thread.sleep(5000);
+				BookingDashboardPageElements.GetFinalizeTab(driver).click();
+				Thread.sleep(5000);
+				BookingDashboardPageElements.Get_FinalizeTab_PayDepositOnly(driver).click();
+				Thread.sleep(5000);
+				CommonFunctions.SwitchToContentFrame(driver);		
+				PaymentTransactionType.PaymentThroughCash(driver);
+				CommonFunctions.SwitchToContentFrame(driver);
+				ReceiptSelectionDialogBoxElements.GetNoneButton(driver).click();
+				assertTrue(true);
+				driver.switchTo().defaultContent();
+				String OrderID = BookingDashboardPageElements.Get_PaymentTab_OrderID(driver).getText();
+				Thread.sleep(3000);
+				BookingEventDashboardPageElements.GetBackToBookingsButton(driver).click();
+				Thread.sleep(3000);
+				BookingEventDashboardPageElements.GetOrderIDColumnSettingIcon(driver).click();
+				filterTable(driver, OrderID);
+				Thread.sleep(5000);
+				BookingEventDashboardPageElements.GetViewButtonOfFirstRow(driver).click();
+				
+				
+			
+		  }
+		  catch(Exception e)
+		  {
+			  System.out.println(e);
+		  }
+		  
+			
+	
 		
-		 
-		String str = "Exp 10 Left";
-		System.out.println(str.substring(4,str.length()-5));
-		System.out.println(str.split("Left"));
-		PDDocument doc = PDDocument.load(new File("C:\\Users\\DELL\\Downloads\\TicketTransaction-21038_07_11_2018.pdf"));
-		int count = doc.getNumberOfPages();
-		System.out.println("count = " + count);
-		
-		System.out.println(CommonFunctions.getCurrentDateInMMddyyyyFormat());
-		System.out.println(CommonFunctions.GetCurrentMonth());
-		System.out.println(CommonFunctions.GetCurrentYear());
-		 str = "/store/shop/productdetails?id=5&productId=16";
-		System.out.println("Index of Product id " + str.indexOf("productId"));
-		System.out.println("Index of Product id " + str.substring(str.indexOf("productId"),str.length()));
-		String str1 = str.substring(str.indexOf("productId"),str.length());
-		String str2 = str1.substring(str1.indexOf("=")+1,str1.length());
-		System.out.println(str2);
-		 driver = CommonFunctions.SetupEnvironment(CommonFunctions.url, "chrome");
-		 Thread.sleep(5000);
-		 CommonFunctions.Login(driver,CommonFunctions.UserName,CommonFunctions.Password);
-		 Thread.sleep(5000);
-		 try
-		 {
-			 f();
-		 }
-		 catch (Exception e)
-		 {
-			 System.out.println(e);
-		 }
-		
+	}
+	public static void filterTable(WebDriver driver,String input) throws InterruptedException {
+
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Filter']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//select[@title='Operator']/..")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//li[text()='Is equal to']")).click();
+		Thread.sleep(2000);
+		CommonFunctions.ActionBuilder_EnterValueInTextbox(driver, driver.findElement(By.xpath("//form[@title ='Show items with value that:']//input[@class='k-formatted-value k-input' and @title='Value']")),input);
+		//CommonFunctions.ActionBuilder_EnterValueInTextbox(driver, driver.findElement(By.xpath("//form[@title ='Show items with value that:']//span[@class='k-widget k-numerictextbox']//input[@class='k-formatted-value k-input' and @title='Value']")),input);
+		//CommonFunctions.ActionBuilder_EnterValueInTextbox(driver, driver.findElement(By.xpath("/html/body/div[9]/div/ul/li[6]/div/ul/li/div/form/div/span[2]/span/input[1]")),input);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[text()='Filter']")).click();
+		Thread.sleep(2000);
+
 	}
 	public static void f() throws Exception {
 		 DashboardPageElements.GetEmployeeRegistersLink(driver).click();
---
+++ src/ObjectRepository/BookingEventDashboardPageElements.java
@@ -0,0 +1,98 @@
+package ObjectRepository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BookingEventDashboardPageElements {
+
+private static WebElement element = null;
+	
+	
+	public static WebElement GetBookingPlusIcon(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//span[@class='dropbtn']/i"));		
+		return element;
+	}
+	public static WebElement GetBookingTab(WebDriver driver)
+	{
+		element =driver.findElement(By.id("booking"));		
+		return element;
+	}
+	public static WebElement GetAvailabitlityTab(WebDriver driver)
+	{
+		element =driver.findElement(By.id("availability"));		
+		return element;
+	}
+	public static WebElement GetSearchButton(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("availability"));		
+		return element;
+	}
+	
+	public static WebElement GetAddBookingOption(WebDriver driver)
+	{
+		element =driver.findElement(By.linkText("Add Booking"));		
+		return element;
+	}
+	public static WebElement GetOrderIDColumnSettingIcon(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//th[@data-field='OrderId']/a[1]"));		
+		return element;
+		//*[@id="BookingGrid"]/div[3]/table/tbody/tr[2]/td[14]/span
+	}
+	public static WebElement GetBackToBookingsButton(WebDriver driver)
+	{
+		element =driver.findElement(By.id("btnGoToMyBookings"));		
+		return element;
+
+	}
+	public static WebElement GetTimeOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[3]"));
+		return element;
+	}
+	public static WebElement GetAssignmentOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[4]"));
+		return element;
+	}
+	public static WebElement GetGuestNameOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[5]"));
+		return element;
+	}
+	public static WebElement GetPackageNameOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[7]"));
+		return element;
+	}
+	public static WebElement GetSiteOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[8]"));
+		return element;
+	}
+	public static WebElement GetCategoryOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[9]"));
+		return element;
+	}
+	public static WebElement GetPaymentStatusOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[10]"));
+		return element;
+	}
+	public static WebElement GetOrderNumberOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[11]/span"));
+		return element;
+	}
+	
+
+	public static WebElement GetViewButtonOfFirstRow(WebDriver driver)
+	{
+		element =driver.findElement(By.xpath("//div[@id='BookingGrid']//div[@class='k-grid-content']//table/tbody/tr[2]/td[14]/span"));
+		return element;
+	}
+	
+}
